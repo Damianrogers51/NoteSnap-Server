fe0cac3a9402cb96ef5ed4d683c03dc3ffd2cc0a
Damian rogers <damianrogers51@gmail.com>
2025-08-13 15:59:14 -0700

    add simple websocket server

---
+++ index.js
@@ -0,0 +1,18 @@
+const io = require('socket.io')(process.env.PORT, {
+    cors: {
+        origin: ['http://localhost:3000', 'https://note-snap-psi.vercel.app'],
+        methods: ['GET', 'POST']
+    },
+    maxHttpBufferSize: 1e8
+});
+
+io.on('connection', (socket) => {
+    const noteId = socket.handshake.headers['note-id'];
+    socket.join(noteId);
+    console.log(`a user connected: ${socket.id}, for room: ${noteId}`);
+
+    socket.on('image', (image) => {
+        socket.to(noteId).emit('image', image);
+        console.log('image emitted to room:', noteId);
+    });
+});
